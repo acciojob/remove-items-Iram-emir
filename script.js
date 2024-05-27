60adb7d23f0cdf9823769dbceec08716f5904c3b
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-27 17:30:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",function() {
+	var button = document.querySelector('input[type="button"]');
+	button.addEventListener('click',function() {
+	var button = document.getElementById('colorSelect');
+		var selectedIndex = selecct.selectIndex;
+		if (selectedIndex !== -1) {
+			select.remove(selectedIndex);
+		}
+	});					    
+});
+						
